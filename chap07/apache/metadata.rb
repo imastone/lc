63c1c8c5891f1e511b5149ec64dcae0bff0b90cd
Alex <imastone@gmail.com>
2015-07-26 15:40:27 -0400

    Chapter 7

---
+++ chap07/apache/metadata.rb
@@ -0,0 +1,7 @@
+name 'apache'
+maintainer 'Alex'
+maintainer_email 'imastone@gmail.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
